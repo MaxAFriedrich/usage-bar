d6b02367dc76034da9f58ccd022554891b301168
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2025-11-09 18:09:43 +0000

    handle all states

--- overspeed_monitor.c
+++ overspeed_monitor.c
@@ -12,6 +12,7 @@
 #define WINDOW_WIDTH	192
 #define WINDOW_HEIGHT	108
 #define MAX_SCREENS	16
+#define SMALL_WINDOW_SIZE	20
 
 /* NotificationState enum values */
 #define STATE_BREAK_OVER	0
@@ -19,6 +20,13 @@
 #define STATE_TYPING		2
 #define STATE_OVERSPEED		3
 #define STATE_BREAK_DUE		4
+
+/* Color definitions from colors.md */
+#define COLOR_BREAK_OVER	0x007051  /* green */
+#define COLOR_BREAK		0x142f8c  /* blue */
+#define COLOR_TYPING		0x8C4914  /* orange */
+#define COLOR_OVERSPEED		0xB70000  /* red */
+#define COLOR_BREAK_DUE		0xFF6D00  /* orange for break_due */
 
 struct screen_window {
 	Window window;
@@ -34,6 +42,7 @@ struct window_state {
 	struct screen_window screens[MAX_SCREENS];
 	int num_screens;
 	int is_visible;
+	int current_state;
 };
 
 static void init_x11(struct window_state *ws)
@@ -58,10 +67,8 @@ static void init_x11(struct window_state *ws)
 		for (i = 0; i < ws->num_screens; i++) {
 			ws->screens[i].width = screens[i].width;
 			ws->screens[i].height = screens[i].height;
-			ws->screens[i].x = screens[i].x_org +
-					   (screens[i].width - WINDOW_WIDTH) / 2;
-			ws->screens[i].y = screens[i].y_org +
-					   (screens[i].height - WINDOW_HEIGHT) / 2;
+			ws->screens[i].x = screens[i].x_org;
+			ws->screens[i].y = screens[i].y_org;
 
 			ws->screens[i].window = XCreateSimpleWindow(
 				ws->display,
@@ -88,8 +95,8 @@ static void init_x11(struct window_state *ws)
 		ws->num_screens = 1;
 		ws->screens[0].width = DisplayWidth(ws->display, screen);
 		ws->screens[0].height = DisplayHeight(ws->display, screen);
-		ws->screens[0].x = (ws->screens[0].width - WINDOW_WIDTH) / 2;
-		ws->screens[0].y = (ws->screens[0].height - WINDOW_HEIGHT) / 2;
+		ws->screens[0].x = 0;
+		ws->screens[0].y = 0;
 
 		ws->screens[0].window = XCreateSimpleWindow(
 			ws->display,
@@ -110,6 +117,7 @@ static void init_x11(struct window_state *ws)
 	}
 
 	ws->is_visible = 0;
+	ws->current_state = -1;
 }
 
 static void show_overspeed_window(struct window_state *ws)
@@ -117,15 +125,20 @@ static void show_overspeed_window(struct window_state *ws)
 	const char *text = "OVERSPEED";
 	XFontStruct *font;
 	int text_width, text_x, text_y;
-	int i;
+	int i, center_x, center_y;
 
-	if (ws->is_visible)
+	if (ws->is_visible && ws->current_state == STATE_OVERSPEED)
 		return;
 
 	for (i = 0; i < ws->num_screens; i++) {
+		center_x = ws->screens[i].x + (ws->screens[i].width - WINDOW_WIDTH) / 2;
+		center_y = ws->screens[i].y + (ws->screens[i].height - WINDOW_HEIGHT) / 2;
+
+		XMoveWindow(ws->display, ws->screens[i].window, center_x, center_y);
+		XResizeWindow(ws->display, ws->screens[i].window, WINDOW_WIDTH, WINDOW_HEIGHT);
 		XMapRaised(ws->display, ws->screens[i].window);
 
-		XSetForeground(ws->display, ws->screens[i].gc, 0xB70000);
+		XSetForeground(ws->display, ws->screens[i].gc, COLOR_OVERSPEED);
 		XFillRectangle(ws->display, ws->screens[i].window,
 			       ws->screens[i].gc, 0, 0,
 			       WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -154,6 +167,7 @@ static void show_overspeed_window(struct window_state *ws)
 
 	XFlush(ws->display);
 	ws->is_visible = 1;
+	ws->current_state = STATE_OVERSPEED;
 }
 
 static void hide_overspeed_window(struct window_state *ws)
@@ -168,6 +182,102 @@ static void hide_overspeed_window(struct window_state *ws)
 
 	XFlush(ws->display);
 	ws->is_visible = 0;
+}
+
+static void show_break_due_window(struct window_state *ws)
+{
+	const char *text = "BREAK";
+	XFontStruct *font;
+	int text_width, text_x, text_y;
+	int i, center_x, center_y;
+
+	if (ws->is_visible && ws->current_state == STATE_BREAK_DUE)
+		return;
+
+	for (i = 0; i < ws->num_screens; i++) {
+		center_x = ws->screens[i].x + (ws->screens[i].width - WINDOW_WIDTH) / 2;
+		center_y = ws->screens[i].y + (ws->screens[i].height - WINDOW_HEIGHT) / 2;
+
+		XMoveWindow(ws->display, ws->screens[i].window, center_x, center_y);
+		XResizeWindow(ws->display, ws->screens[i].window, WINDOW_WIDTH, WINDOW_HEIGHT);
+		XMapRaised(ws->display, ws->screens[i].window);
+
+		XSetForeground(ws->display, ws->screens[i].gc, COLOR_BREAK_DUE);
+		XFillRectangle(ws->display, ws->screens[i].window,
+			       ws->screens[i].gc, 0, 0,
+			       WINDOW_WIDTH, WINDOW_HEIGHT);
+
+		XSetForeground(ws->display, ws->screens[i].gc, 0x7C7C7C);
+
+		font = XLoadQueryFont(ws->display,
+				      "-*-helvetica-bold-r-*-*-32-*-*-*-*-*-*-*");
+		if (!font)
+			font = XLoadQueryFont(ws->display, "fixed");
+
+		if (font)
+			XSetFont(ws->display, ws->screens[i].gc, font->fid);
+
+		text_width = XTextWidth(font, text, strlen(text));
+		text_x = (WINDOW_WIDTH - text_width) / 2;
+		text_y = WINDOW_HEIGHT / 2 + 16;
+
+		XDrawString(ws->display, ws->screens[i].window,
+			    ws->screens[i].gc, text_x, text_y,
+			    text, strlen(text));
+
+		if (font)
+			XFreeFont(ws->display, font);
+	}
+
+	XFlush(ws->display);
+	ws->is_visible = 1;
+	ws->current_state = STATE_BREAK_DUE;
+}
+
+static void show_small_indicator(struct window_state *ws, int state)
+{
+	int i;
+	unsigned long color;
+
+	switch (state) {
+	case STATE_BREAK_OVER:
+		color = COLOR_BREAK_OVER;
+		break;
+	case STATE_BREAK:
+		color = COLOR_BREAK;
+		break;
+	case STATE_TYPING:
+		color = COLOR_TYPING;
+		break;
+	default:
+		return;
+	}
+
+	if (ws->is_visible && ws->current_state == state)
+		return;
+
+	for (i = 0; i < ws->num_screens; i++) {
+		XMoveWindow(ws->display, ws->screens[i].window, 
+			    ws->screens[i].x, ws->screens[i].y);
+		XResizeWindow(ws->display, ws->screens[i].window, 
+			      SMALL_WINDOW_SIZE, SMALL_WINDOW_SIZE);
+		XMapRaised(ws->display, ws->screens[i].window);
+
+		XSetForeground(ws->display, ws->screens[i].gc, color);
+		XFillRectangle(ws->display, ws->screens[i].window,
+			       ws->screens[i].gc, 0, 0,
+			       SMALL_WINDOW_SIZE, SMALL_WINDOW_SIZE);
+	}
+
+	XFlush(ws->display);
+	ws->is_visible = 1;
+	ws->current_state = state;
+}
+
+static void hide_all_windows(struct window_state *ws)
+{
+	hide_overspeed_window(ws);
+	ws->current_state = -1;
 }
 
 static void cleanup_x11(struct window_state *ws)
@@ -236,11 +346,23 @@ int main(void)
 
 			printf("Received state: %d\n", state);
 
-			if (state == STATE_OVERSPEED) {
+			switch (state) {
+			case STATE_OVERSPEED:
 				printf("OVERSPEED detected!\n");
 				show_overspeed_window(&ws);
-			} else {
-				hide_overspeed_window(&ws);
+				break;
+			case STATE_BREAK_DUE:
+				printf("BREAK DUE!\n");
+				show_break_due_window(&ws);
+				break;
+			case STATE_BREAK_OVER:
+			case STATE_BREAK:
+			case STATE_TYPING:
+				show_small_indicator(&ws, state);
+				break;
+			default:
+				hide_all_windows(&ws);
+				break;
 			}
 		}
 
